2b41f5d5efec4ea9a5248013858b31ee18333251
keerthana <keerthananeeluru2001@gmail.com>
2023-07-27 16:34:57 +0530

    commited primitivetypeandExpressions file

---
+++ PrimitiveTypesAndExpressions/PrimitiveTypesAndExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypesAndExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a=true;
+            byte b = 100;
+            sbyte sb = -10;
+            char c= 'a';
+            decimal d = 1.03m;
+            double du = 122.3;
+            string s = "hi";
+            float f = 122.3f;
+            
+        }
+    }
+}
