aafd6f6dc97e3e46f5bf045d8c56adcf88d8ca45
keerthana <keerthananeeluru2001@gmail.com>
2023-07-27 20:34:16 +0530

    adding Recursion

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    public class Program
+    {
+        public static void PrinViaRecurion(string[] stu,int index)
+        {
+            if (index < 0 || index >= stu.Length)
+            {
+                return;
+            }
+            Console.WriteLine(stu[index]);
+            PrinViaRecurion(stu,index+1);
+        }
+        public static void RunApp()
+        {
+            string[] stu = { "Alice", "Eva", "Alen", "Dave", "Charlie" };
+            Console.WriteLine("Printing student names");
+            PrinViaRecurion(stu,0);
+        }
+        static void Main(string[] args)
+        {
+            RunApp();
+            Console.ReadKey();
+        }
+    }
+}
