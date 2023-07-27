7afa86665b04b0494b2ad156b235fb19f08bd414
keerthana <keerthananeeluru2001@gmail.com>
2023-07-27 19:03:41 +0530

    adding stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        public static void RunApp()
+        {
+            Stack stack=new Stack();
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            int top1=stack.Peek();
+            Console.WriteLine("Top value "+top1);
+            int pop1=stack.Pop();
+            Console.WriteLine("popped value "+pop1);
+            int top2 = stack.Peek();
+            Console.WriteLine("Top value " + top2);
+            
+        }
+        static void Main(string[] args)
+        {
+            RunApp();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public Node top;
+        public Stack() {
+            top = null;
+        }
+        public void Push(int d)
+        {
+            Node n = new Node(d);
+            n.next = top;
+            top = n;
+
+        }
+        public int Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            int d=top.data;
+            top= top.next;
+            return d;
+
+        }
+        public int Peek()
+        {
+            if(top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return top.data;
+        }
+
+    }
+}
