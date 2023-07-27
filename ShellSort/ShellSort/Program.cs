17a662d1ae7e02f3a20aa4ecc4a60b82ab13a0fb
keerthana <keerthananeeluru2001@gmail.com>
2023-07-27 20:39:46 +0530

    adding Shellsort

---
+++ ShellSort/ShellSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+
+    internal class Program
+    {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+            int gap = n / 2;
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    int temp = array[i];
+                    int j = i;
+
+                    while (j >= gap && array[j - gap] > temp)
+                    {
+                        array[j] = array[j - gap];
+                        j -= gap;
+                    }
+
+                    array[j] = temp;
+                }
+
+                gap /= 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] marks = { 64, 34, 25, 42, 52, 81, 90 };
+            Console.WriteLine("Original Array: " + string.Join(", ", marks));
+
+            Sort(marks);
+            Console.WriteLine("Sorted Array: " + string.Join(", ", marks));
+            Console.ReadKey();
+        }
+    }
+}
