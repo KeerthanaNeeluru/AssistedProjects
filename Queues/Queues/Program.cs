ffb1b2cfc5bea1eda190d8bd2345828a14d3fb13
keerthana <keerthananeeluru2001@gmail.com>
2023-07-28 16:18:44 +0530

    ading queue

--- Queues/Queues/Program.cs
+++ Queues/Queues/Program.cs
@@ -10,6 +10,15 @@ namespace Queues
     {
         static void Main(string[] args)
         {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            Console.WriteLine("peek "+queue.Peek());
+            Console.WriteLine("REmove value from queue "+queue.Dequeue());
+            Console.WriteLine("peek " + queue.Peek());
+            Console.ReadKey();
         }
     }
 }
